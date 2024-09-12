13eb65da823d9e41bad7e3821a9503026a2bbd99
Ilham Oktavian <99379666+Ilhamokta74@users.noreply.github.com>
2024-09-12 14:51:34 +0700

    Part 8

---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $ownerRole = Role::create([
+            'name' => 'owner'
+        ]);
+
+        $buyerRole = Role::create([
+            'name' => 'buyer'
+        ]);
+
+        $user = User::create([
+            'name' => 'Pemilik',
+            'email' => 'pemilik@gmail.com',
+            'password' => bcrypt('Admin#123'),
+        ]);
+
+        $user->assignRole($ownerRole);
+    }
+}
